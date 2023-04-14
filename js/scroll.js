1f42a40f569d891affc8a647158223127d8a7f9d
Cebbbbbbbbb <109560705+Cebbbbbbbbb@users.noreply.github.com>
2023-04-14 14:30:01 -0500

    agregando el proyecto

---
+++ js/scroll.js
@@ -0,0 +1,22 @@
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("abajo",window.scrollY>0);
+  })
+  //EFECTO APARICION GRADUAL
+  ScrollReveal().reveal('.content', {
+    duration: 2000,
+    origin: 'left',
+    distance: '400px'
+});
+
+//BOTON PARA VER MAS PRODUCTOS
+  function ViewMore(){
+    window.location.href="#content";
+  }
+//Tooltips (titulos ensima de otros)  
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+//Login
+function login(){
+  window.location.href="login/login.html";
+}
